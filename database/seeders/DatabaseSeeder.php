6bdfc71ebc49d6dddb9358b2d619ea73d9215f36
Mpie <info@mpie.nl>
2026-02-18 17:16:19 +0100

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 final class DatabaseSeeder extends Seeder
 {
@@ -13,10 +14,14 @@ final class DatabaseSeeder extends Seeder
     {
         $this->call(RolesAndPermissionsSeeder::class);
 
-        $user = User::factory()->create([
-            'name' => 'Mpie',
-            'email' => 'info@mpie.nl',
-        ]);
+        $user = User::firstOrCreate(
+            ['email' => 'info@mpie.nl'],
+            [
+                'name' => 'Mpie',
+                'password' => Hash::make('changeme!2026'),
+                'email_verified_at' => now(),
+            ],
+        );
 
         $user->assignRole('super_admin');
     }
